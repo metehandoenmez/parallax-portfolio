c5b6e8f212e9457108b4db5a35ddc1e5dd8324b7
metehandoenmez <metehandoenmez@gmail.com>
2023-08-04 15:43:47 +0300

    edited styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import Footer from './assets/Components/Footer'
 import { useEffect, useState } from 'react'
 import Loading from './assets/Components/Loading'
+import {useRef} from 'react'
+import NavButton from './assets/Components/NavButton'
 
 function App() {
+  const pageRef = useRef<any>(null)
+
   const bg = '/bg2.png'
 
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
-      setIsLoading(false);
+      setIsLoading(false);  
     }, 3000);
   }, []);
 
@@ -22,29 +26,35 @@ function App() {
       ) : (
         <div id="content">
           <div className='wipbanner'><i>Work in Progress</i></div>
-          <Parallax pages={4.2}>
-          <ParallaxLayer sticky={{start: 0.4, end: 2.4}}>
-            <img className='balloon' src="/balloon.png" alt="" />
+          <NavButton pageRef={pageRef} />
+          <Parallax pages={4.2} ref={pageRef}>
+          <ParallaxLayer sticky={{start: 0.4, end: 1.5}}>
+            <img className='balloon' src="/cactus.png" alt="" />
           </ParallaxLayer>
           
-          <ParallaxLayer offset={0} speed={0.5} factor={7} style={{
-            backgroundImage: `url(${bg})`,
-            backgroundSize: `cover`
+          <ParallaxLayer offset={0} speed={0.5} factor={8} style={{
+            // backgroundImage: `url(${bg})`,
+            // backgroundSize: `cover`
+            background: "#eee7e1",
           }}>
           </ParallaxLayer>
-          <ParallaxLayer offset={0.2} speed={0.05}>
-            <div className='text2'>Metehan Dönmez
-            <h4>Full Stack Developer<span className='blinking'>_</span></h4></div>
+          <ParallaxLayer offset={0.15} speed={0.05}>
+            <div className='header'>Metehan Dönmez
+            <h4><span className='blinking'>_</span>Full Stack Developer</h4></div>
             {/* <div className='explorecont'><button className='explore'>Explore</button>
             </div> */}
           
           </ParallaxLayer>
+
+          <ParallaxLayer offset={1.6} speed={0.4}>
+          <i className='socialsHeader'>WORKS</i>
+          </ParallaxLayer>
   
-          <ParallaxLayer offset={3.5} speed={1}>
+          <ParallaxLayer offset={3.4} speed={1}>
           <i className='socialsHeader'>SOCIALS</i>
           </ParallaxLayer>
   
-          <ParallaxLayer offset={3.8} speed={2}>
+          <ParallaxLayer offset={3.5} speed={2}>
           <Footer></Footer>
           </ParallaxLayer>
         </Parallax>
---
+++ src/assets/Components/NavButton.tsx
@@ -0,0 +1,15 @@
+export default function NavButton({pageRef}) {
+  return (
+    <div className='navbutton'>
+      <div className='navbutton1'>
+        <button onClick={()=> pageRef.current.scrollTo(0)}>Home</button>
+      </div>
+      <div className='navbutton2'>
+      <button onClick={()=> pageRef.current.scrollTo(0.9)}>Works</button>
+      </div>
+      <div className='navbutton3'>
+      <button onClick={()=> pageRef.current.scrollTo(3.2)}>Socials</button>
+      </div>
+    </div>
+  )
+}
